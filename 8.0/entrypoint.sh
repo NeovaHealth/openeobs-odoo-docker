cc07b9cee21f405d41d16bf750ead13fbe574dda
Colin Wren <colin.wren@bjss.com>
2017-08-07 16:57:54 +0100

    Canne use virtualenv with setup.py so removing for now

--- 8.0/entrypoint.sh
+++ 8.0/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -e
-source /opt/nh/venv/bin/activate
 
 # set the postgres database host, port, user and password according to the environment
 # and pass them as arguments to the odoo process if not present in the config file
